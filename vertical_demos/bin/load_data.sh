f6c95e43a9430401f66b97b31b62277fb01daa91
Tom Scott <thomaskwscott@gmail.com>
2024-03-01 10:38:15 +0000

    verticals demo update

--- vertical_demos/bin/load_data.sh
+++ vertical_demos/bin/load_data.sh
@@ -36,6 +36,13 @@ docker-compose exec kafka1 kafka-topics \
   --if-not-exists \
   --create \
   --partitions 1
+docker-compose exec kafka1 kafka-topics \
+  --bootstrap-server kafka1:9092 \
+  --topic transactions \
+  --if-not-exists \
+  --create \
+  --partitions 1
+
 
 cat data/Finance.avsc | docker-compose exec -T schema-registry tee -a Finance.avsc
 cat data/Gaming.avsc | docker-compose exec -T schema-registry tee -a Gaming.avsc
@@ -43,9 +50,21 @@ cat data/Healthcare.avsc | docker-compose exec -T schema-registry tee -a Healthc
 cat data/Manufacturing.avsc | docker-compose exec -T schema-registry tee -a Manufacturing.avsc
 cat data/Retail.avsc | docker-compose exec -T schema-registry tee -a Retail.avsc
 cat data/Tech.avsc | docker-compose exec -T schema-registry tee -a Tech.avsc
+cat data/transactions.avsc | docker-compose exec -T schema-registry tee -a transactions.avsc
 
-for a in {1..10}
-do
+
+tar -xvzf data/transactions.tar.gz -C data
+cat data/transactions.txt |
+    docker-compose exec -T schema-registry kafka-avro-console-producer \
+      --bootstrap-server kafka1:9092 \
+      --topic transactions \
+      --producer-property linger.ms=10 \
+      --producer-property batch.size=1000 \
+      --property schema.registry.url=http://schema-registry:8081 \
+      --property value.schema.file=transactions.avsc
+
+#for a in {1..10}
+#do
 cat data/Finance.json |
     docker-compose exec -T schema-registry kafka-avro-console-producer \
       --bootstrap-server kafka1:9092 \
@@ -82,4 +101,4 @@ cat data/Tech.json |
       --topic tech_markers \
       --property schema.registry.url=http://schema-registry:8081 \
       --property value.schema.file=Tech.avsc
-done
+#done
